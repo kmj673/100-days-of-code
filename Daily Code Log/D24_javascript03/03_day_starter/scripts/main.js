523cb01641259c57ecfb2beaadd4908f5cb750b7
kmj673 <kmj673@gmail.com>
2021-01-12 00:45:32 +0800

    commit on 'master'

---
+++ Daily Code Log/D24_javascript03/03_day_starter/scripts/main.js
@@ -0,0 +1,41 @@
+// this is your main.js script
+
+let firstName = "minju"
+let lastName = "Kim"
+let country="korea"
+let city = "seoul"
+let age= 24
+let isMarried=false
+let year = 2021
+
+
+let k= new Date();
+year= k.getFullYear()
+month=k.getMonth()+1
+date=k.getDate()
+day=k.getDay()
+hour=k.getHours()
+min=k.getMinutes()
+milli=k.getTime()
+
+/*5..
+
+let x2 = prompt('enter x2')
+let x1 = prompt('enter x1')
+
+let y2=2*parseInt(x2)-2
+let y1=2*parseInt(x1)-2
+
+slope= (y2-y1)/(x2-x1)
+
+let x=0
+let yi=2*x-2
+
+let y=0
+let xi=0.5*y+1
+
+
+alert(`slope is ${slope}. y intercept is ${yi}. x intercept is ${xi}`)
+
+*/
+
